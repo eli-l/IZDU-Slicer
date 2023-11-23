37d4c31707cf24b760eb207f73aa396ae7e4be3b
Ilja Lapkovskis <ilja.lapkovskis@gmail.com>
2023-11-23 22:00:52 +0200

    Add watermaks

--- src/image_slicer.rs
+++ src/image_slicer.rs
@@ -1,6 +1,7 @@
 use anyhow::{Error, Result};
 use image::{DynamicImage, GenericImageView, ImageBuffer, Rgba};
 use reqwest;
+use rusttype::{Font, Scale};
 
 #[derive(Debug)]
 struct Dimension {
@@ -46,9 +47,82 @@ fn slice_images_view(
         let x = (pic as u32 % 2) * new_img_size.width;
         let y = (pic as u32 / 2) * new_img_size.height;
         let view = img.view(x, y, new_img_size.width, new_img_size.height);
-        *new_img = view.to_image();
+
+        let font_data = include_bytes!("../resources/OpenSans-Regular.ttf");
+        let font = Font::try_from_bytes(font_data as &[u8]).unwrap();
+        let text = "github.com/eli-l/IZDU-Slicer";
+        let scale = Scale::uniform(24.0);
+        let wm_ready = DynamicImage::ImageRgba8(render_text_to_image(&font, scale, text)).resize(
+            600,
+            300,
+            image::imageops::FilterType::Nearest,
+        );
+
+        *new_img = add_watermark(view.to_image(), wm_ready, 0.8)
     });
     output
+}
+
+fn render_text_to_image(font: &Font, scale: Scale, text: &str) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
+    let v_metrics = font.v_metrics(scale);
+    let glyphs: Vec<_> = font
+        .layout(text, scale, rusttype::point(0.0, v_metrics.ascent))
+        .collect();
+
+    let glyphs_height = v_metrics.ascent - v_metrics.descent;
+    let glyphs_width = glyphs
+        .iter()
+        .map(|g| g.position().x as f32 + g.unpositioned().h_metrics().advance_width)
+        .fold(0.0 as f32, |a, b| a.max(b))
+        .ceil() as u32;
+
+    let mut image = ImageBuffer::new(glyphs_width, glyphs_height as u32);
+
+    for glyph in glyphs {
+        if let Some(bounding_box) = glyph.pixel_bounding_box() {
+            glyph.draw(|x, y, v| {
+                let x = x as i32 + bounding_box.min.x;
+                let y = y as i32 + bounding_box.min.y;
+                let v = (v * 255.0) as u8;
+                image.put_pixel(x as u32, y as u32, Rgba([v, v, v, v]));
+            });
+        }
+    }
+
+    image
+}
+
+fn add_watermark(
+    mut img: ImageBuffer<Rgba<u8>, Vec<u8>>,
+    watermark: DynamicImage,
+    alpha: f32,
+) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
+    let (w, h) = (watermark.width(), watermark.height());
+    let (iw, ih) = (img.width(), img.height());
+    let x = (iw - w) / 2;
+    let y = (ih - h) / 2;
+
+    for i in 0..w {
+        for j in 0..h {
+            let wm = watermark.get_pixel(i, j);
+            let image = *img.get_pixel(x + i, y + j);
+
+            // let alpha1 = image[3] as f32 / 255.0;
+            // let alpha2 = wm[3] as f32 / 255.0;
+            let alpha2 = f32::max(alpha, 0.5);
+            let alpha1 = 1.0 - alpha2;
+            let alpha = alpha1 + alpha2 * (1.0 - alpha1);
+            let px = image::Rgba([
+                ((alpha1 * image[0] as f32) + (alpha2 * wm[0] as f32 * (1.0 - alpha1))) as u8,
+                ((alpha1 * image[1] as f32) + (alpha2 * wm[1] as f32 * (1.0 - alpha1))) as u8,
+                ((alpha1 * image[2] as f32) + (alpha2 * wm[2] as f32 * (1.0 - alpha1))) as u8,
+                (alpha * 255.0) as u8,
+            ]);
+            img.put_pixel(x + i, y + j, px);
+        }
+    }
+    // image::imageops::overlay(&mut img, &watermark, x as i64, y as i64);
+    img
 }
 
 // Split image by copying pixels one by one - initial approach.
